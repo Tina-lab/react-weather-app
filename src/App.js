af235f51efbb2251e7f3428a4ca9d0a3124ec682
Tina <y634huan@uwaterloo.ca>
2023-04-11 19:31:46 -0400

    formed my elementary weather app.

--- src/App.js
+++ src/App.js
@@ -1,95 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
 import MainWeatherWindow from './components/MainWeatherWindow';
 import CityInput from './components/CityInput';
-import WeatherBox from './components/WeatherBox';
 
-class App extends React.Component {
-  state = {
-    city: undefined,
-
-    // days contains objects with the following properties:
-    // date, weather_desc, icon, temp
-    days: new Array(5)
-  };
-
-  // creates the day objects and updates the state
-  updateState = data => {
-    const city = data.city.name;
-    const days = [];
-    const dayIndices = this.getDayIndices(data);
-
-    for (let i = 0; i < 5; i++) {
-      days.push({
-        date: data.list[dayIndices[i]].dt_txt,
-        weather_desc: data.list[dayIndices[i]].weather[0].description,
-        icon: data.list[dayIndices[i]].weather[0].icon,
-        temp: data.list[dayIndices[i]].main.temp
-      });
-    }
-
-    this.setState({
-      city: city,
-      days: days
-    });
-  };
-
-  // tries to make an API call with the given city name and triggers state update
-  makeApiCall = async city => {
+const App = () => {
+  const [CityWeather, SetCityWeather] = useState(['Toronto', '11d']);
+  console.log(CityWeather)
+  const ApiCall = async (city) => {
     const api_data = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=6557810176c36fac5f0db536711a6c52`
     ).then(resp => resp.json());
+    console.log(api_data);
+    console.log(api_data.list[0].weather[0].icon);
 
-    if (api_data.cod === '200') {
-      await this.updateState(api_data);
+    if (api_data.cod === '200'){
+      await SetCityWeather([city, api_data.list[0].weather[0].icon])
       return true;
-    } else return false;
-  };
-
-  // returns array with Indices of the next five days in the list
-  // from the API data (every day at 12:00 pm)
-  getDayIndices = data => {
-    let dayIndices = [];
-    dayIndices.push(0);
-
-    let index = 0;
-    let tmp = data.list[index].dt_txt.slice(8, 10);
-
-    for (let i = 0; i < 4; i++) {
-      while (
-        tmp === data.list[index].dt_txt.slice(8, 10) ||
-        data.list[index].dt_txt.slice(11, 13) !== '15'
-      ) {
-        index++;
-      }
-      dayIndices.push(index);
-      tmp = data.list[index].dt_txt.slice(8, 10);
-    }
-    return dayIndices;
-  };
-
-  render() {
-    const WeatherBoxes = () => {
-      const weatherBoxes = this.state.days.slice(1).map(day => (
-        <li>
-          <WeatherBox {...day} />
-        </li>
-      ));
-
-      return <ul className='weather-box-list'>{weatherBoxes}</ul>;
-    };
-
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <MainWeatherWindow data={this.state.days[0]} city={this.state.city}>
-            <CityInput city={this.state.city} makeApiCall={this.makeApiCall.bind(this)} />
-            <WeatherBoxes />
-          </MainWeatherWindow>
-        </header>
-      </div>
-    );
+    }else return false;
   }
+
+
+  return (
+    <div className = 'App'>
+      <header className = 'App-header'>
+        <MainWeatherWindow CityWeather = {CityWeather}/>
+        <CityInput makeApiCall = {ApiCall} />
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/MainWeatherWindow.js
+++ src/components/MainWeatherWindow.js
@@ -1,45 +1,32 @@
-import React from 'react';
+import React from 'react'
 import './MainWeatherWindow.css';
 
-export default class MainWeatherWindow extends React.Component {
-  render(props) {
-    const Title = this.props.city ? null : <h1 className='title'>Weather Forecast</h1>;
-
-    return (
-      <div className='main'>
-        <div className='inner-main'>
-          {Title}
-          <img
-            src={
-              this.props.data
-                ? require(`../images/${this.props.data.icon}.svg`)
-                : require('../images/01d.svg')
-            }
-            alt='sun'
-            style={{
-              visibility: this.props.city ? 'visible' : 'hidden',
-              opacity: this.props.city ? '1' : '0'
-            }}
-          />
-
-          <div
-            className='today'
-            style={{
-              visibility: this.props.city ? 'visible' : 'hidden',
-              opacity: this.props.city ? '1' : '0'
-            }}
-          >
-            <span>Today</span>
-            <h1>{this.props.city}</h1>
-            <p>
-              Temperature: {this.props.data ? Math.round(this.props.data.temp - 273.15) : 0}
-              °C
-            </p>
-            <p>{this.props.data ? this.props.data.weather_desc.toLowerCase() : ''}</p>
-          </div>
-        </div>
-        {this.props.children}
+const MainWeatherWindow = ({CityWeather}) => {
+  const Title = <h1 className='title'>Weather Forecast</h1>;
+  console.log(CityWeather)
+  console.log('print the rendering image')
+  console.log(CityWeather
+    ? '../images/${CityWeather[1]}.svg'
+    : '../images/01d.svg')
+  return (
+    <div className='main'>
+      <div className='inner-main'>
+        {/* {Title} */}
+        <img
+          src={CityWeather
+            ? require(`../images/${CityWeather[1]}.svg`)
+            : require('../images/01d.svg')
+          }
+          // src = {require('../images/01d.svg')}
+          alt='sun'
+          style={{
+            visibility: 'visible' ,
+            opacity:  '1' 
+          }}
+        /> 
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default MainWeatherWindow
